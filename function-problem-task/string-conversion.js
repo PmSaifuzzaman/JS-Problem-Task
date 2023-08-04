d2e540687f43ec97884dbf1bae887d3989f96862
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-04 21:46:56 +0600

    String Conversion

---
+++ function-problem-task/string-conversion.js
@@ -0,0 +1,10 @@
+
+// String conversion to lowercase and Uppercase
+const userName = 'PmSaifuzzaman';
+const userInput = 'pmsaifuzzaman';
+if( userName.toLocaleLowerCase() == userInput.toLocaleLowerCase()){
+    console.log('valid User');
+}
+else{
+    console.log('Invalid User');
+}
