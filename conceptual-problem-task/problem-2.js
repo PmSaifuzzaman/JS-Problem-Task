98d30fbcf601abd723f8039b38b7330d5f4ec5f3
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-07 14:11:56 +0600

    problem -2 split method

--- conceptual-problem-task/problem-2.js
+++ conceptual-problem-task/problem-2.js
@@ -3,6 +3,8 @@
 ফাংশন নেম দিতে হবে isJavaScriptFile । এই ফাংশনে প্যারামিটার হিসেবে নিবে একটি স্ট্রিং(String) যেটি হবে একটি ফাইল নেম (যেমনঃ ‘index.js’)। যদি এটি জাভাস্ক্রিপ্ট ফাইল হয় তোমাকে true রিটার্ন করতে হবে আর যদি না হয় তাহলে false রিটার্ন করতে হবে 
 */
 
+
+// with built in endswith function
 function isJavaScriptFile(fileName){
     if(typeof fileName !=='string'){
         return 'Please enter a valid filename (string)....';
@@ -18,3 +20,27 @@ function isJavaScriptFile(fileName){
     }
 }
 console.log(isJavaScriptFile('problem.js'));
+
+
+
+
+
+
+
+// with split file name 
+function isJavaScriptFile(fileName){
+    if(typeof fileName !== 'string'){
+        return 'Please enter a string type valid filename';
+    }
+    else{
+        const arr = fileName.split(".");
+        const lastElement = arr.pop();
+        if(lastElement == 'js'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
+console.log(isJavaScriptFile('index.js'));
