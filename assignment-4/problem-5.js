d60ba4ce39196798ad69a2bf7992b83403e00bbe
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-09 23:54:14 +0600

    problem 5

---
+++ assignment-4/problem-5.js
@@ -0,0 +1,40 @@
+function canPay(changeArray, totalDue) {
+    if(Array.isArray(changeArray)!==true || typeof totalDue != 'number'){
+        return 'Please enter first element array and second element number';
+    }
+    else if(changeArray.length === 0 ){
+        return 'The array you enter is empty, please enter a valid number array';
+    }
+    else{
+        let totalChange = 0;
+        for (const element of changeArray) {
+            totalChange += element;
+            if(typeof element != 'number'){
+                return 'Please enter number type element in the array';
+            }
+        }
+        return totalChange >= totalDue;  
+    }
+    
+}
+console.log(canPay([],10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
